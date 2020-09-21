5e57b9951444c03e45d23dcf20035976f5be9176
Combatd <combatdstar@gmail.com>
2020-09-21 03:39:35 -0700

    Test naive to find out it is the product of two args

--- social_network_magic_trick/casestudy.py
+++ social_network_magic_trick/casestudy.py
@@ -13,6 +13,7 @@ print(naive(5, 10)) # 5 * 10 = 50
 print(naive(25, 75)) # 25 * 75 = 1875
 
 '''
+    My Hypothesis:
     This naive function is actually a mathematical proof that a product
     of two numbers multiplied together is actually the sum of one number
     being added to itself by the second number's value times.
