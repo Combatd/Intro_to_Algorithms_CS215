8bcaac589ae7cf42d420d3a1457452f77737dbf5
Combatd <combatdstar@gmail.com>
2020-09-21 03:50:29 -0700

    Analyze Time Complexity of #naive(a, b)

--- social_network_magic_trick/casestudy.py
+++ social_network_magic_trick/casestudy.py
@@ -17,4 +17,10 @@ print(naive(25, 75)) # 25 * 75 = 1875
     This naive function is actually a mathematical proof that a product
     of two numbers multiplied together is actually the sum of one number
     being added to itself by the second number's value times.
+
+    Time Complexity:
+    We have a while loop running our inputs until x decrements all the way to 0
+    On a graph, this would be represented as t = cn.
+    
+    O(n) represents our time complexity.
 '''
