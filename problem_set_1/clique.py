ddee7775ea9a90c421e3fbd058481504a33ad5c5
Combatd <combatdstar@gmail.com>
2020-10-13 18:20:30 -0700

    Analyze  units of time #clique(n)

--- problem_set_1/clique.py
+++ problem_set_1/clique.py
@@ -5,3 +5,21 @@ def clique(n):
             print(i, "is friends with", j)
 
 clique(4)
+
+'''
+print a string
+second operation range of 4 (0, 1, 2, 3)
+evaluate range 0
+evaluate range 1
+print a string
+evaluate range 2
+
+print
+print 
+range of 3 evaluate
+print 
+print 
+print 
+12 units of time
+6 lines returned in the terminal
+'''
